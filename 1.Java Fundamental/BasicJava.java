ae64ee7bae9b66dd240812f301c8c6352e0b3087
Jared Keyes <Jared.keyes3@gmail.com>
2018-01-12 19:24:19 -0500

    Updated basic java

--- 1.Java Fundamental/BasicJava.java
+++ 1.Java Fundamental/BasicJava.java
@@ -5,4 +5,46 @@ public class BasicJava {
             }
         return "Done.";
     }
+    public String oddLoop(int number) {
+        for ( int i = 1; i <= number; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+        return "Done.";
+    }
+    public String sumOf(int number) {
+        int sum = 0;
+        for ( int i = 0; i <= number; i++) {
+            System.out.println("New number: " + i);
+            sum = sum + i;
+            System.out.println("Sum: " + sum);
+        }
+        return "Done.";
+    }
+    public String iterate(int[] array) {
+        for ( int i = 0; i < array.length; i++) {
+        System.out.println(array[i]);
+        }
+        return "Done.";
+    }
+    public String findMax(int[] array) {
+        int max = array[0];
+        for ( int i = 0; i < array.length; i++) {
+            if ( array[i] > max) {
+                max = array[i];
+            }
+        }
+        System.out.println(max);
+        return "Done.";
+    }
+    public String average(int[] array) {
+        int sum = 0;
+        for ( int i= 0; i < array.length; i++) {
+            sum = sum + array[i];
+        }
+        int average = sum/array.length;
+        System.out.println(average);
+        return "Done.";
+    }
 }
--- 1.Java Fundamental/BasicJavaTest.java
+++ 1.Java Fundamental/BasicJavaTest.java
@@ -1,7 +1,18 @@
 public class BasicJavaTest {
     public static void main(String[] args){
         BasicJava bj = new BasicJava();
-        String run = bj.loop(255);
-        System.out.println(run);
+        String loop = bj.loop(255);
+        String oddLoop = bj.oddLoop(255);
+        String sumOf = bj.sumOf(255);
+        int[] arr = {-1,-2,3,-4,-5,-10};
+        String iterate = bj.iterate(arr);
+        String findMax = bj.findMax(arr);
+        String average = bj.average(arr);
+        System.out.println(loop);
+        System.out.println(oddLoop);
+        System.out.println(sumOf);
+        System.out.println(iterate);
+        System.out.println(findMax);
+        System.out.println(average);
     }
 }
